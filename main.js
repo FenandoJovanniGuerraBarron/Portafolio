d383df3a5edfcc785dbe496cf9436ffbc66439dd
FernandoJovanniGuerraBarron <fernando_jova@hotmail.com>
2022-08-27 23:27:24 -0500

    Version 2.0

---
+++ main.js
@@ -0,0 +1,7 @@
+const iconMenu = document.querySelector("#iconMenu");
+const menu = document.querySelector("#menu");
+
+iconMenu.addEventListener("click", function () {
+    menu.classList.toggle("menu__show");
+});
+ 
