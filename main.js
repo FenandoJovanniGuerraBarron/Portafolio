5a35465ce2f9f973ba94aede56d664999bd4685a
FernandoJovanniGuerraBarron <fernando_jova@hotmail.com>
2022-09-02 17:02:08 -0500

    arreglo de menu

--- main.js
+++ main.js
@@ -4,4 +4,40 @@ const menu = document.querySelector("#menu");
 iconMenu.addEventListener("click", function () {
     menu.classList.toggle("menu__show");
 });
+
+const Home = document.querySelector("#Home-C");
+
+Home.addEventListener("click", function () {
+    menu.classList.toggle("menu__show");
+});
+ 
+const acercaDeMi = document.querySelector("#Acerca-de-mi-C");
+
+acercaDeMi.addEventListener("click", function () {
+    menu.classList.toggle("menu__show");
+});
+
+const misHabilidades = document.querySelector("#Mis-Habilidades-C");
+
+misHabilidades.addEventListener("click", function () {
+    menu.classList.toggle("menu__show");
+});
+
+const experiencia = document.querySelector("#Experiencia-C");
+
+experiencia.addEventListener("click", function () {
+    menu.classList.toggle("menu__show");
+});
+ 
+const portafolio = document.querySelector("#Portafolio-C");
+
+portafolio.addEventListener("click", function () {
+    menu.classList.toggle("menu__show");
+});
+ 
+const contacto = document.querySelector("#Contacto-C");
+
+contacto.addEventListener("click", function () {
+    menu.classList.toggle("menu__show");
+});
  
